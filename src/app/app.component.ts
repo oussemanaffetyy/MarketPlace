60c47e2471eeb8716a7aecda70f299b64487a20f
OUSSEMA <oussemanaffetyy@gmail.com>
2024-09-27 16:20:45 +0100

    Commit before Angular update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+declare var $: any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'MarketPlace';
+
+  ngOnInit(): void {
+    // Ensure that jQuery is executed after the DOM is ready
+    $(document).ready(() => {
+      // Custom script using jQuery
+      let body = <HTMLDivElement>document.body;
+      let script = document.createElement('script');
+      script.src = '../assets/js/script.js';
+      script.async = true;
+      body.appendChild(script);
+    });
+  }
 }
